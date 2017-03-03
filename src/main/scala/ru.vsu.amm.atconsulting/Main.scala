47cc60af4ec7be66811a241f9053d10205f5461f
FeodorM <Feodor.Mann@gmail.com>
2017-03-03 17:05:06 +0300

    многое вынесено из main в отдельные функции

--- src/main/scala/ru.vsu.amm.atconsulting/Main.scala
+++ src/main/scala/ru.vsu.amm.atconsulting/Main.scala
@@ -1,43 +1,54 @@
 package ru.vsu.amm.atconsulting
 
+import com.sun.org.apache.xpath.internal.functions.WrongNumberArgsException
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
   def main(args: Array[String]): Unit = {
+    if (args.length < 3)
+      throw new WrongNumberArgsException("Должно быть 3 аргумента")
+
     val conf = new SparkConf().setMaster("local").setAppName("My App")
     val sc = new SparkContext(conf)
 
-    if (args.length < 3) return
+    val milesTable = sc.broadcast(getMapWithMiles(sc, sc.textFile(args(1))))
+    val drivers = getDrivers(sc, sc.textFile(args(0)), milesTable)
 
-    val driversAndMiles = sc.textFile(args(1))
-      .map(line => {
-        val words = line split ","
-        val miles = words.slice(3, 16)
-          .foldLeft((0, true)) {
-            case ((acc, true), nextVal) => (acc + nextVal.toInt, false)
-            case ((acc, false), _) => (acc, true)
-          }._1
-        (words(0), miles)
-      })
+    prepareForOutput(sc, drivers).saveAsTextFile(args(2))
 
-    val milesTable = sc.broadcast(driversAndMiles.collectAsMap)
+    sc.stop()
+  }
 
-    sc.textFile(args(0))
-      .filter(!_.contains("normal"))
+  def getMapWithMiles(sc: SparkContext, rdd: RDD[String]): collection.Map[String, Int] =
+    rdd.map(line => {
+      val words = line split ","
+      val miles = words.slice(3, 16)
+        .foldLeft((0, true)) {
+          case ((acc, true), nextVal) => (acc + nextVal.toInt, false)
+          case ((acc, false), _) => (acc, true)
+        }._1
+      (words(0), miles)
+    })
+    .collectAsMap
+
+  def getDrivers(sc: SparkContext, rdd: RDD[String],
+                 broadcast: Broadcast[collection.Map[String, Int]]): RDD[Driver] =
+    rdd.filter(!_.contains("normal"))
       .map(line => {
         val words = line split ","
         (words(1), words(1))
       })
       .aggregateByKey(Driver(""))({ case (Driver(_, num, _), id) =>
-        Driver(id, num + 1, milesTable.value.getOrElse(id, -99))
+        Driver(id, num + 1, broadcast.value.getOrElse(id, -99))
       }, { case (Driver(id, num1, miles), Driver(_, num2, _)) =>
         Driver(id, num1 + num2, miles)
       })
-      .map({ case (id, Driver(_, numberOfAcc, miles)) =>
-        (id, numberOfAcc, miles, miles / numberOfAcc)
-      })
-      .saveAsTextFile(args(2))
+      .map({ case (_, driver) => driver })
 
-    sc.stop()
-  }
+  def prepareForOutput(sc: SparkContext, rdd: RDD[Driver]): RDD[(String, Int, Int, Int)] =
+    rdd.map({ case Driver(id, numberOfAcc, miles) =>
+      (id, numberOfAcc, miles, miles / numberOfAcc)
+    })
 }
